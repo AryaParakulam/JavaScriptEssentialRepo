51ba706af3d6312ce3cd34507f227ca90c1984f3
Arya <aryababu1357@gmail.com>
2025-11-08 01:52:14 -0500

    Restaurant Menu

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,19 @@
+const breakfastMenu = ['Pancakes- $1', 'Eggs Benedict- $5', 'Oatmeal-$3', 'Frittata-$2'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
+        document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+let mainCourseItem = '';
+mainCourseMenu.forEach((item, index) => {
+mainCourseItem += `<p>Item ${index + 1}: ${item}</p>`;});
+document.getElementById('maincourseMenuItems').innerHTML = mainCourseItem;
+
+let dessertItem = '';
+for (let i = 0; i < dessertMenu.length; i++) {
+    dessertItem += `<p>Item ${i + 1}: ${dessertMenu[i]}</p>`;}
+document.getElementById('dessertMenuItems').innerHTML = dessertItem;
+
+
+
