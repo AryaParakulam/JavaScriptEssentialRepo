3bfa2034f3bcd2f710d0660bc624d9078e5c5087
Arya <aryababu1357@gmail.com>
2025-11-10 00:53:10 -0500

    Debug code

---
+++ debugCode/debug_code.js
@@ -0,0 +1,53 @@
+    
+    function performOperation() {
+    // Get user input from input fields
+    let num1 = parseInt(document.getElementById('input1').value);
+    let num2 = parseInt(document.getElementById('input2').value);
+    // Check if inputs are valid numbers
+    if (!isNaN(num1) && !isNaN(num2)) {
+    // Perform the operation
+                    let resultm = multiply(num1, num2);
+                    // Display the result
+                    displayResult(resultm);
+
+                    let resultd = divide(num1, num2);
+                    // Display the result
+                    displayResult(resultd);
+
+                    let resulta = add(num1, num2);
+                    // Display the result
+                    displayResult(resulta);
+
+                } else {
+                    displayResult('Please enter valid numbers');
+                }
+            }
+
+            function multiply(a, b) {
+                // Introduce a debugger statement to pause execution
+                debugger;
+
+                // Multiply the numbers
+                return a * b;
+            }
+            function divide(a, b) {
+                // Introduce a debugger statement to pause execution
+                debugger;
+
+                // divide the numbers
+                return a / b;
+            }
+            function add(a, b) {
+                // Introduce a debugger statement to pause execution
+                debugger;
+
+                // add the numbers
+                return a + b;
+            }
+
+            function displayResult(result) {
+                // Display the result in the paragraph element
+                const resultElement = document.getElementById('result');
+                resultElement.textContent = `The result is: ${result}`;
+            }
+        
